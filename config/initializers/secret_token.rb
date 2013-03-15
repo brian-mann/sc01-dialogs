3904e374fd4c530ba333f305b59e33a53c84f01b
Brian Mann <brian@osysit.com>
2013-03-15 18:13:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sc01Dialogs::Application.config.secret_token = 'a4288bdf7807dc823358639904851449f6dba95bcb64bf61aaeb4b91dff6f5428aec6e643658994bac725f896051ad467422aaabbf3c70c741089c3c7df084b6'
